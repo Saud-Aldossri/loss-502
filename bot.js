d3ffa28194852b9ba4ac7d23b62d2451e31198ed
Loss502 <43408050+Loss502@users.noreply.github.com>
2018-09-22 14:54:23 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ const prefix = "&";
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+client.login('NDkzMDIwNjk3MzEzMDE3ODY2.Doe47Q.psu0ukizKnExznUDet3Aw8tf4po'); //
