b300b596bfa7994bea6041986bcffb3da1f9cf27
Loss502 <43408050+Loss502@users.noreply.github.com>
2018-09-23 17:14:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+ const prefix = "+";
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -10,6 +10,26 @@ client.on('message', message => {
     	message.reply('pong');
   	}
 });
+
+
+
+
+
+  client.on('message', message => {
+   if(message.content.startsWith(prefix + "invite")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} لقد قمت بدعوه ${inviteCount} دعوه.`);
+});
+  }
+});
+  
+
+
+
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
