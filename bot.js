84bf6eb1cfd0164abe3ef4902b490506911112fd
Loss502 <43408050+Loss502@users.noreply.github.com>
2018-10-05 12:55:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -552,7 +552,10 @@ client.on("message", message => { // Leaked by [ @Loss ]
 
 
 
-
+// help command
+if {msg === prefix + `HELP` || msg === prefix + `COMMANDS`} { // Checks to see if they soid help OR Commands
+message.channel.send{commandsList}
+}} ;
 
 
 
