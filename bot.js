0008b0cf14082eeb8e275f965b07581a8564e36e
Loss502 <43408050+Loss502@users.noreply.github.com>
2018-10-12 15:54:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
 const Canvas = require('canvas');
@@ -111,13 +110,14 @@ client.on('message', async message => {
 
 
 
-client.on('message', message=> {
-    if (message.author.bot) return;
-    if (message.isMentioned(client.user))
-    {
-    message.reply("سـم اخوي ؟");
+client.on('ebnklb',function(ebnklb) {
+    
+    if(ebnklb.content.startsWith("<303197992809267202>")) {
+        ebnklb.channel.send('Hey Im **Silver!**  A Nice Bot Developed By:`Loss`')
+        ebnklb.channel.send('My Prefix `+')
+
     }
-})
+});
 
 
 
