5ebfbfc60578f3904727cd6715c1ac680b05d7e2
Loss502 <43408050+Loss502@users.noreply.github.com>
2018-10-17 12:47:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -326,30 +326,6 @@ client.on('ready',  () => {
 
 
 
-client.on('message', message => {
-    let args = message.content.split(" ").slice(1);
-    if (message.author.bot) return;
-    if (!message.channel.guild) return;
-    if (message.content.startsWith(prefix + 'clear')) {
-
-        if (isNaN(args[0])) return message.channel.send('**Please supply a valid amount of messages to purge**');
-        if (args[0] > 100) return message.channel.send('**Please supply a number less than 100**');
-
-        message.channel.bulkDelete(args[0])
-            .then(messages => message.channel.send(`**Successfully deleted \`${messages.size}/${args[0]}\` messages**`).then(msg => msg.delete({
-                timeout: 5000
-            })))
-    }
-});
-
-
-
-
-
-
-
-
-
 
 client.on("message", message => {
   let men = message.mentions.users.first();
